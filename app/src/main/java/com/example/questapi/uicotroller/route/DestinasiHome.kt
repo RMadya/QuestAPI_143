eb432ffb2381541c52546fc41924856ae0c7585c
RMadya <kereyoben75@gmail.com>
2025-12-18 20:54:40 +0700

    Objek DestinasiHome berfungsi sebagai peta identitas atau alamat resmi untuk layar beranda aplikasi.

---
+++ app/src/main/java/com/example/questapi/uicotroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi.uicotroller.route
+
+import com.example.questapi.R
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.nama
+}
