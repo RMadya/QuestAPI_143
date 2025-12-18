97e525c5b3b33bbf4ec67caea5e51ef55e57ab67
RMadya <kereyoben75@gmail.com>
2025-12-18 20:49:09 +0700

    Ini adalah kontrak yang mendefinisikan operasi data apa saja yang tersedia di aplikasi.
    Fungsi: Menggunakan kata kunci suspend agar fungsi ini dapat dijalankan di dalam coroutine (secara asinkron) sehingga tidak membuat aplikasi membeku (freeze) saat menunggu respons dari internet.

---
+++ app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,14 @@
+package com.example.questapi.repositori
+
+import com.example.questapi.apiservice.ServiceApiSiswa
+import com.example.questapi.modeldata.DataSiswa
+
+interface RepositoriDataSiswa{
+    suspend fun getSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+//    suspend fun getSatuSiswa(id: Int): DataSiswa
+//    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
+//    suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void>
+}
+
